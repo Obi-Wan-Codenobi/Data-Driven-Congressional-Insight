c00f58373e46b821109e9c2142a8de1281b37e57
EWenthe <ewenthe@tamu.edu>
2024-11-04 19:56:48 -0600

    bm25 skeleton

--- data_collection/main.py
+++ data_collection/main.py
@@ -5,12 +5,17 @@ from utils.load_vote_data import load_vote_data
 
 def main():
     file_prefix = "./../../congress/data/118/votes/"
-    
+    test_folder = "../bill_texts/"
     year_start = 2016
     year_end = 2024
    
+    user_query = input("What topic would you like to reference? ")
+
     load_vote_data(file_prefix, year_start, year_end) 
     initiate_bill_load()
+    
+
+
 
 if __name__ == "__main__":
     main()
