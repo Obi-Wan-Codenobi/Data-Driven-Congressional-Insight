cb1bb0bec2960ef7cd23da583fd7be579b89e711
Jaxon Singleton <101373646+Obi-Wan-Codenobi@users.noreply.github.com>
2024-11-11 13:42:33 -0600

    Cleaned house

--- api/routes/__init__.py
+++
@@ -1,8 +0,0 @@
-__all__ = ["router"]
-
-from sys import prefix
-from fastapi import APIRouter
-from .home import home_router
-
-router = APIRouter()
-router.include_router(home_router, prefix="/api/home")
