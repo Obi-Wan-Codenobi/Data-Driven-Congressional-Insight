ec5cdfab68104cc25a9020f22f461acf063c6a8c
Jaxon Singleton <101373646+Obi-Wan-Codenobi@users.noreply.github.com>
2024-11-20 01:15:35 -0600

    Test bm25 with 40k docs

--- data_collection/main.py
+++ data_collection/main.py
@@ -5,7 +5,7 @@ from utils.initiate_bill_load import initiate_bill_load
 from utils.load_vote_data import load_and_merge_all_votes
 from utils.bm25 import BM25
 from utils.dumbyloader import *
-from utils.document import get_document_data
+from utils.document import get_document_data, load_xml_to_text
 from utils.load_bills_data import load_and_merge_all_bills
 from utils.tftypes import TFTYPES
 import os
@@ -59,10 +59,23 @@ def main():
         votes = get_vote_documents(path)
 
         bills_keys = set(bills.keys()) if bills else set()
-        votes_keys = set(votes.keys()) if votes else set()
+        #votes_keys = set(votes.keys()) if votes else set()
 
-        common_keys = bills_keys & votes_keys
-        print(f"Intersected keys: {len(common_keys)}")
+        #common_keys = bills_keys & votes_keys
+        #print(f"Intersected keys: {len(common_keys)}")
+        print('WRITING')
+        load_xml_to_text(bills, "./tmp/")
+        
+        # test query
+        document_data = get_document_data("./tmp/")
+        user_query = input("What topic would you like to reference? ")
+        scored_documents = executeBM25(user_query, document_data)
+        top_x = 5  
+        for i in range(min(top_x, len(scored_documents))):
+            doc, score = scored_documents[i]
+            print(f"Document: {doc.title}, Score: {score}")
+        
+        
         
         
         sys.exit()
